9ad6dc0ae8eb466873a497952f4bebdf989ebe40
Pooyan Khanjankhani <pooyankhan@gmail.com>
2024-04-29 13:59:37 +0330

    update logger

--- src/logger.ts
+++ src/logger.ts
@@ -8,50 +8,47 @@ type Level =
   | "info"
   | "debug";
 
-export class Log {
-  public constructor(
-    public readonly name: string,
-    public readonly parent: Log | null = null
-  ) {}
-
-  public toString(): string {
-    const prefix = this.parent ? this.parent.toString() : "";
-
-    return `${prefix}: ${this.name}`;
-  }
-}
-
 export class Logger {
-  public constructor(
-    private readonly outputStream: NodeJS.WritableStream = process.stdout
-  ) {}
+  public readonly context: string[];
 
-  public emerg(message: string | Log) {
+  public constructor(
+    context: string[] | string | null = null,
+    private readonly outputStream: NodeJS.WritableStream = process.stdout
+  ) {
+    this.context = context
+      ? typeof context === "string"
+        ? [context]
+        : context
+      : [];
+  }
+
+  public emerg(message: string) {
     this.log("emerg", message);
   }
-  public alert(message: string | Log) {
+  public alert(message: string) {
     this.log("alert", message);
   }
-  public crit(message: string | Log) {
+  public crit(message: string) {
     this.log("crit", message);
   }
-  public err(message: string | Log) {
+  public err(message: string) {
     this.log("err", message);
   }
-  public warning(message: string | Log) {
+  public warning(message: string) {
     this.log("warning", message);
   }
-  public notice(message: string | Log) {
+  public notice(message: string) {
     this.log("notice", message);
   }
-  public info(message: string | Log) {
+  public info(message: string) {
     this.log("info", message);
   }
-  public debug(message: string | Log) {
+  public debug(message: string) {
     this.log("debug", message);
   }
 
-  private log(level: Level, message: string | Log) {
-    this.outputStream.write(`${level} - ${message}`);
+  private log(level: Level, message: string) {
+    const ctx = this.context.join(": ");
+    this.outputStream.write(`${ctx}${level} - ${message}`);
   }
 }
